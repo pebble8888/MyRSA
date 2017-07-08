a5a9934d3d7f47ac2799e4378db41efa6bbc3d9b
pebble8888 <pebble8888@gmail.com>
2017-07-08 16:32:19 +0900

    wip

---
+++ MyRSA/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  MyRSA
+//
+//  Created by pebble8888 on 2017/05/27.
+//  Copyright © 2017年 pebble8888. All rights reserved.
+//
+
+import UIKit
+import SwiftyRSA
+
+/*
+extension Data {
+    var hexDescription:String {
+        return map { String(format: "%02hhx", $0) }.joined()
+    }
+}
+ */
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        do {
+            //let publickey:PublicKey = try PublicKey(pemNamed: "public-key")
+            let privatekey:PrivateKey = try PrivateKey(pemNamed: "private-key")
+            //let message:ClearMessage = try ClearMessage(string: "HogeFuga", using: .utf8) 
+            // let encrypted:EncryptedMessage = try message.encrypted(with: publickey, padding: .PKCS1)
+            
+            let url:URL = Bundle.main.url(forResource: "encrypted_msg", withExtension: "bin")!
+            let data:Data = try Data(contentsOf: url)
+            let encrypted:EncryptedMessage = EncryptedMessage(data: data)
+            
+            //print("\(encrypted.data.hexDescription)")
+            let decrypted:ClearMessage = try encrypted.decrypted(with: privatekey, padding: .PKCS1)
+            print("\(try decrypted.string(encoding: .utf8))")
+        } catch {
+            fatalError()
+        }
+        
+    }
+
+}
+
---
+++ Pods/Target Support Files/Pods-MyRSATests/Pods-MyRSATests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyRSATests : NSObject
+@end
+@implementation PodsDummy_Pods_MyRSATests
+@end
